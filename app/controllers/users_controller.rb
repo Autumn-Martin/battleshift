46feb04c5d3b7d42e84211bf0ca24aa351f48a44
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-09 17:13:38 -0600

    Add dream note

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
 
   def show
-    conn = Faraday.new(url: "http://localhost:3000/api/v1") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-    user_id = params[:id]
-    response = conn.get("users/#{user_id}")
-    attributes = JSON.parse(response.body, symbolize_names: true)
-    @user = User.new(attributes)
+      conn = Faraday.new(url: "http://localhost:3000/api/v1") do |faraday| #change to GetUrl.by_environment later
+        faraday.adapter Faraday.default_adapter
+      end
+      user_id = params[:id]
+      response = conn.get("users/#{user_id}")
+
+      attributes_hash = JSON.parse(response.body, symbolize_names: true)
+      @user = User.new(attributes_hash)
   end
-
-
 
 end
