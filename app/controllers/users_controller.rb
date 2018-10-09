7c8f261cccc6a40e335f81a6c2dc884059efabc8
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-09 16:17:13 -0600

    Takes out pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
 
   def show
-      conn = Faraday.new(url: "http://localhost:3000/api/v1") do |faraday|
-        faraday.adapter Faraday.default_adapter
-      end
-      user_id = params[:id]
-      response = conn.get("users/#{user_id}")
-      # binding.pry
-       attributes = JSON.parse(response.body, symbolize_names: true)
-       @user = User.new(attributes)
+    conn = Faraday.new(url: "http://localhost:3000/api/v1") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+    user_id = params[:id]
+    response = conn.get("users/#{user_id}")
+    attributes = JSON.parse(response.body, symbolize_names: true)
+    @user = User.new(attributes)
   end
 
 
