75d1c046fb362c45b1aa0ff3560476b174bd1368
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-09 21:02:42 -0600

    Use ActiveSupport::StringInquirer magic

--- spec/services/get_url_spec.rb
+++ spec/services/get_url_spec.rb
@@ -5,21 +5,21 @@ describe GetUrl do
     subject { described_class.by_environment }
 
     context 'test env' do
-      before { allow(Rails.env).to receive(:test?).and_return(true)}
+      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('test'))}
       it 'returns local url' do
         expect(subject).to eq 'http://localhost:3000/api/v1'
       end
     end
 
     context 'development env' do
-      before { allow(Rails.env).to receive(:development?).and_return(true)}
+      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development'))}
       it 'returns local url' do
         expect(subject).to eq 'http://localhost:3000/api/v1'
       end
     end
 
     context 'production env' do
-      before { allow(Rails.env).to receive(:production?).and_return(true)}
+      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production'))}
       it 'returns heroku url' do
         expect(subject).to eq 'https://limitless-taiga-55796.herokuapp.com/'
       end
