e3cbd7b589351cb35498b5026d4a906f6043e350
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-10 16:39:01 -0600

    WIP production

--- app/services/user_services.rb
+++ app/services/user_services.rb
@@ -13,7 +13,8 @@ class UserServices
 
   private
     def conn
-      Faraday.new(url: ENV["PATH"]) do |faraday|
+      # binding.pry
+      Faraday.new(url: ENV["root-url"]) do |faraday|
         faraday.adapter Faraday.default_adapter
       end
     end
