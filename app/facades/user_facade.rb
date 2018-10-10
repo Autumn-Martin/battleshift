42b05fc18771146fad5c8aa579414ddbe6ed02bc
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-10 11:19:15 -0600

    Complete UserFacade & UserServices poros

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -4,21 +4,11 @@ class UserFacade
   end
 
   def user_name
-    get_attributes("users/#{@id}")[:name]
+    UserServices.new("users/#{@id}").name
   end
 
   def user_email
-    get_attributes("users/#{@id}")[:email]
+    UserServices.new("users/#{@id}").email
   end
 
-  private
-    def conn
-      Faraday.new(url: GetUrl.by_environment) do |faraday|
-        faraday.adapter Faraday.default_adapter
-      end
-    end
-
-    def get_attributes(url)
-      JSON.parse(conn.get(url).body, symbolize_names: true)
-    end
 end
