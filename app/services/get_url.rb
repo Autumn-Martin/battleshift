3a14918ace8db7e4b9dc436842d4730b861a7182
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-09 20:05:03 -0600

    Add get url logic to use specified urls in different environments

---
+++ app/services/get_url.rb
@@ -0,0 +1,10 @@
+class GetUrl
+
+  def self.by_environment
+    if Rails.env.test? || Rails.env.development?
+      "http://localhost:3000/api/v1"
+    elsif Rails.env.production?
+      "https://limitless-taiga-55796.herokuapp.com/"
+    end
+  end
+end
