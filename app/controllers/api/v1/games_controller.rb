3b64d5d872b750c8d50009c614f623523edb3a44
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-10 23:31:01 -0600

    Resolve test error by adding rescue: render status 400 when RecordNotFound

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class GamesController < ActionController::API
+      rescue_from ActiveRecord::RecordNotFound, with: :render_400
+
+      def render_400
+        render status: 400
+      end
+
       def show
         game = Game.find(params[:id])
         render json: game
