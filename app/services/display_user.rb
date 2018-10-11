8f83f154a1fe3f3788920f3de28f46c50d46718c
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-11 11:30:43 -0600

    Creates new user.

--- app/services/display_user.rb
+++ app/services/display_user.rb
@@ -1,6 +1,15 @@
 class DisplayUser
+  def initialize(user_data)
+    @user_data = user_data
+  end
 
+  def user_name #refactor move to DisplayUser
+    user.name
+  end
 
+  def user_email #refactor move to DisplayUser
+    user.email
+  end
 
 
 end
