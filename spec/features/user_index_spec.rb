2e257b14b17200553113a0882e9a8a3c2aff1f05
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-10 23:53:08 -0600

    Un-skip test

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe 'user index' do
-   xit 'should show guest the user info' do
+   it 'should show guest the user info' do
 
      # user = User.create!(name: "Josiah Bartlet", email: "jbartlet@example.com")
      # binding.pry
-     user = User.create!(name: "Han Solo", email: "hansolo@email.com")
+     user = User.new(name: "Han Solo", email: "hansolo@email.com")
+
      visit "/users"
      expect(page).to have_content("Josiah Bartlet")
      expect(page).to have_content("jbartlet@example.com")
