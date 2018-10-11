67237d02751545713abd594054fdeabf63a55df0
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-10 23:55:11 -0600

    Add route for users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  # resources :users, only: [:show]
-  get '/users/:id', to: 'users#show'
+  resources :users, only: [:show, :index]
+
   namespace :api do
     namespace :v1 do
       resources :games, only: [:show] do
