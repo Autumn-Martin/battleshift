e22dab59afd1a7d7f96a677e9daf1ba95a54bbb2
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-12 17:24:18 -0600

    wip vcr

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 describe 'user index' do
-   it 'should show guest all users info' do
+  it 'should show guest all users info' do
+     # VCR.use_cassette("services/find_co_members") do
+     #        members = PropublicaService.find_house_members("CO")
+     #        member = members.first
 
-     visit "/users"
+    VCR.use_cassette("battle_shift_user_index") do
+      users = UserService.new.get_users
+
+
+      # visit "/users"
      expect(page).to have_content("Josiah Bartlet")
      expect(page).to have_content("jbartlet@example.com")
      expect(page).to have_content("Josh Lyman")
      expect(page).to have_content("jlyman@example.com")
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,10 +2,15 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
+
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+end
 
 SimpleCov.start "rails"
 
