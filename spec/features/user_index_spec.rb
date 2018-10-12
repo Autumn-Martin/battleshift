c95b70986c3024a28a5e6e010bd485bc238cc082
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-12 17:44:28 -0600

    WIP VCR

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -2,19 +2,13 @@ require 'rails_helper'
 
 describe 'user index' do
   it 'should show guest all users info' do
-     # VCR.use_cassette("services/find_co_members") do
-     #        members = PropublicaService.find_house_members("CO")
-     #        member = members.first
-
-    VCR.use_cassette("battle_shift_user_index") do
-      users = UserService.new.get_users
-
-
-      # visit "/users"
-     expect(page).to have_content("Josiah Bartlet")
-     expect(page).to have_content("jbartlet@example.com")
-     expect(page).to have_content("Josh Lyman")
-     expect(page).to have_content("jlyman@example.com")
+    VCR.use_cassette("all_users") do
+      visit "/users"
     end
+
+   expect(page).to have_content("Josiah Bartlet")
+   expect(page).to have_content("josiah@example.com")
+   expect(page).to have_content("Josh Lyman")
+   expect(page).to have_content("jlyman@example.com")
   end
 end
