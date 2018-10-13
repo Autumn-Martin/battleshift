9cbf4421f896ecbedf9bf0928f6a543c1f055c3d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-12 19:20:46 -0600

    Test with no vcr

--- spec/features/user_can_register_spec.rb
+++ spec/features/user_can_register_spec.rb
@@ -1,20 +1,20 @@
-# require "rails_helper"
-#
-# describe 'As a guest user' do
-#   it 'should be able to register a user' do
-#     visit "/"
-#     click_on "Register"
-#     expect(current_path).to eq("/register")
-#     fill_in :user_email, with: "toby@whitehouse.com"
-#     fill_in :user_name, with: "Toby"
-#     fill_in :user_password, with: "cats"
-#     fill_in :user_password_confirmation, with: "cats"
-#     click_on "Submit"
-#     expect(current_path).to eq("/dashboard")
-#     expect(page).to have_content("Logged in as Toby")
-#     expect(page).to have_content("This account has not yet been activated. Please check your email.")
-#   end
-# end
+require "rails_helper"
+
+describe 'As a guest user' do
+  it 'should be able to register a user' do
+    visit "/"
+    click_on "Register"
+    expect(current_path).to eq("/register")
+    fill_in :user_email, with: "toby@whitehouse.com"
+    fill_in :user_name, with: "Toby"
+    fill_in :user_password, with: "cats"
+    fill_in :user_password_confirmation, with: "cats"
+    click_on "Submit"
+    expect(current_path).to eq("/dashboard")
+    expect(page).to have_content("Logged in as Toby")
+    expect(page).to have_content("This account has not yet been activated. Please check your email.")
+  end
+end
 # As a guest user
 # When I visit "/"
 # And I click "Register"
