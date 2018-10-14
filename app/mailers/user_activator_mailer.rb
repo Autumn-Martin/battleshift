6dd1239b2b29741c15913d300bd7a139c2662fc3
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-13 18:25:17 -0600

    Adds inform to mailer class.

--- app/mailers/user_activator_mailer.rb
+++ app/mailers/user_activator_mailer.rb
@@ -1,2 +1,8 @@
 class UserActivatorMailer < ApplicationMailer
+
+  def inform(user, user_contact)
+    @user = user
+    mail(to: user_contact, subject: "Activate Battleshift Account")
+  end
+
 end
