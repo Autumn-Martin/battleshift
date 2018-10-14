5bd426678e94ebd939fa7c07194273255b9d8c3b
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-14 09:17:05 -0600

    Fix bugs so that user can be created.

--- app/controllers/user_activation_controller.rb
+++ app/controllers/user_activation_controller.rb
@@ -1,7 +1,7 @@
-class UserActivationController < ApplicationController
-  def create
-    UserActivatorMailer.inform(current_user, params[:email]).deliver_now
-    flash[:notice] = "Successfully sent activation email."
-    redirect_to root_url
-  end
-end
+# class UserActivationController < ApplicationController
+#   def create
+#     UserActivatorMailer.inform(current_user, params[:email]).deliver_now
+#     flash[:notice] = "Successfully sent activation email."
+#     redirect_to root_url
+#   end
+# end
--- app/mailers/user_activator_mailer.rb
+++ app/mailers/user_activator_mailer.rb
@@ -2,6 +2,7 @@ class UserActivatorMailer < ApplicationMailer
 
   def inform(user, user_contact)
     @user = user
+    @url = "localhost.com/users/#{user.id}/activate"
     mail(to: user_contact, subject: "Activate Battleshift Account")
   end
 
