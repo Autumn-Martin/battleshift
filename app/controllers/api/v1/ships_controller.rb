fc3beb14d17a2fb55637dee3597a50edc1c29f7c
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-16 16:40:25 -0600

    WIP adds created method for ship controller.:

--- app/controllers/api/v1/ships_controller.rb
+++ app/controllers/api/v1/ships_controller.rb
@@ -1,18 +1,14 @@
 class Api::V1::ShipsController < ApiController
 
   def create
-    # binding.pry
-    ship1 = Ship.new(2)
-    ship2 = Ship.new(3)
-    ships = [ship1, ship2]
-    render json: ships
-    # render json: Ship.new(3)
-
+    game = Game.find_by(id: params[:game_id])
+    ship = Ship.new(ship_params)
+    render json:  game
   end
 
-  # private
-  #
-  #   def ship_params
-  #     params.permit(:length, :damage, :start_space, :end_space)
-  #   end
+  private
+
+    def ship_params
+      params.require(:ship).permit(:length, :damage, :start_space, :end_space, :ship_size)
+    end
 end
