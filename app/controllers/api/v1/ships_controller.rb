016efe85b7ccca88c78d5d227da24b7d7158f60b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-16 15:21:27 -0600

    Wip, creating ships.

--- app/controllers/api/v1/ships_controller.rb
+++ app/controllers/api/v1/ships_controller.rb
@@ -2,12 +2,17 @@ class Api::V1::ShipsController < ApiController
 
   def create
     # binding.pry
-    render json: Ship.new(ship_params)
+    ship1 = Ship.new(2)
+    ship2 = Ship.new(3)
+    ships = [ship1, ship2]
+    render json: ships
+    # render json: Ship.new(3)
+
   end
 
-  private
-
-    def ship_params
-      params.permit(:length, :damage, :start_space, :end_space)
-    end
+  # private
+  #
+  #   def ship_params
+  #     params.permit(:length, :damage, :start_space, :end_space)
+  #   end
 end
