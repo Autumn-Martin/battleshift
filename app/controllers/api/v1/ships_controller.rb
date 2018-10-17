72b9574b62d846025abc121b27da9eada9cb8c4d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-16 18:15:18 -0600

    refactors ship controller.

--- app/controllers/api/v1/ships_controller.rb
+++ app/controllers/api/v1/ships_controller.rb
@@ -1,32 +1,48 @@
 class Api::V1::ShipsController < ApiController
 
   def create
-    game = Game.find_by(id: params[:game_id])
-    ship_1 = Ship.new(ship_params[:ship_size])
-    ship_1_info = {board: game.player_1_board,
-                   ship: ship_1,
-                   start_space: ship_params[:start_space],
-                   end_space: ship_params[:end_space]}
-
-    ship_1_placer = ShipPlacer.new(ship_1_info)
+    @game = Game.find_by(id: params[:game_id])
     ship_1_placer.run
-
-    ship_2 = Ship.new(ship_params[:ship_size])
-    ship_2_info = {board: game.player_2_board,
-                   ship: ship_2,
-                   start_space: ship_params[:start_space],
-                   end_space: ship_params[:end_space]}
-
-    ship_2_placer = ShipPlacer.new(ship_2_info)
-    # binding.pry
     ship_2_placer.run
-
-    render json:  game, message: "Successfully placed ship with a size of #{ship_1.length}. You have 1 ship(s) to place with a size of #{ship_1.length-1}."
+    render json:  @game, message: messages(ship_1)
   end
 
   private
 
     def ship_params
       params.require(:ship).permit(:length, :damage, :start_space, :end_space, :ship_size)
+    end
+
+    def ship_1
+      Ship.new(ship_params[:ship_size])
+    end
+
+    def ship_1_placer
+      ship_1_info = {board: @game.player_1_board,
+                     ship: ship_1,
+                     start_space: ship_params[:start_space],
+                     end_space: ship_params[:end_space]}
+
+      ShipPlacer.new(ship_1_info)
+    end
+
+    def ship_2
+      Ship.new(ship_params[:ship_size])
+    end
+
+    def ship_2_placer
+      ship_2_info = {board: @game.player_2_board,
+                    ship: ship_2,
+                    start_space: ship_params[:start_space],
+                    end_space: ship_params[:end_space]}
+      ShipPlacer.new(ship_2_info)
+    end
+
+    def messages(ship)
+      if ship.length == 3
+        "Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2."
+      else
+        "Successfully placed ship with a size of 2. You have 0 ship(s) to place."
+      end
     end
 end
