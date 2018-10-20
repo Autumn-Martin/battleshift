7e0d9a9f0efb6ce3e117e4c541de36f0ea77aca5
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-20 13:41:35 -0600

    Refactor test for updating user email

--- spec/features/user_update_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe 'user update path' do
-  let!(:user) { create(:user, email: "bat@z.com")}
-  subject { user.update(email: "octo@i.com")}
-  it 'should update a users email' do
-    expect{subject}.to change{user.reload.email}.from("bat@z.com").to("octo@i.com")
-  end
-end
