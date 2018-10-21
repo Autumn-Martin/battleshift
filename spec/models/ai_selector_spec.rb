9fef920f9a136d8c090a2fec7d8f902263880f2d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-20 22:26:08 -0600

    ai space selector test.

---
+++ spec/models/ai_selector_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'ai_space_selector' do
+  it 'exists' do
+    board = Board.new(2)
+    ai = AiSpaceSelector.new(board)
+    ai.fire!
+
+    expect(ai).to be_a(AiSpaceSelector)
+    expect(board.board_health).to eq(2)
+  end
+end
